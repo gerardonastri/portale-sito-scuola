3fce342a0474c32c841a1e6bf7f57fe44bcffd93
gerardonastri <gerardonastri06@gmail.com>
2023-11-24 12:06:52 +0100

    better

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import './Navbar.css'
+import './Home.css'
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+
+const clientId = "52946809134-dqfc0cvfd2ik9dvo74ik1pe4i2j870cn.apps.googleusercontent.com"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <GoogleLogin
+      onSuccess={credentialResponse => {
+        const decoded = jwtDecode(credentialResponse.credential);
+        console.log(decoded);
+      }}
+      onError={(error) => {
+        console.log(error);
+      }}
+    />
   )
 }
 
