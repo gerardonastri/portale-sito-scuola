fe3a3cbd1900e0539c614dd7a7b51c3bd510e6c4
gerardonastri <gerardonastri06@gmail.com>
2023-11-26 13:16:10 +0100

    bc url

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:8000/api";
-// const BASE_URL = "https://portale-sito-scuola-api.onrender.com/api";
+// const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://portale-sito-scuola-api.onrender.com/api";
 
 export const axiosReq = axios.create({
   baseURL: BASE_URL,
