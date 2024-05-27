95994ae7930e77ec84e96b324dedb629cbe4ff3c
gerardonastri <gerardonastri06@gmail.com>
2024-05-27 16:17:47 +0200

    fixing firebase error

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,131 +1,162 @@
-import React from 'react'
-import './Edit.css'
-import Navbar from '../../components/Navbar/Navbar'
-import {useParams} from 'react-router-dom'
+import React from "react";
+import "./Edit.css";
+import Navbar from "../../components/Navbar/Navbar";
+import { useParams } from "react-router-dom";
 
+import { axiosReq } from "../../utils/apiCalls";
+import { useEffect, useState } from "react";
 
-
-import { axiosReq } from '../../utils/apiCalls';
-import { useEffect, useState } from 'react';
-
-import storage from '../../utils/firebase';
+import storage from "../../utils/firebase";
 
 const Edit = () => {
+  //item
+  const [name, setName] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [iscritti, setIscritti] = useState(null);
+  const [img, setImg] = useState(null);
+  const [classe, setClasse] = useState(null);
+  const [capienzaMassima, setCapienzaMassima] = useState(null);
+  const [durata, setDurata] = useState(null);
+  const [slot, setSlot] = useState(null);
+  const [organizzatore, setOrganizzatore] = useState(null);
+  let { id } = useParams();
 
-    //item
-    const [name, setName] = useState(null)
-    const [desc, setDesc] = useState(null)
-    const [iscritti, setIscritti] = useState(null)
-    const [img, setImg] = useState(null)
-    const [classe, setClasse] = useState(null)
-    const [capienzaMassima, setCapienzaMassima] = useState(null)
-    const [durata, setDurata] = useState(null)
-    const [slot, setSlot] = useState(null)
-    const [organizzatore, setOrganizzatore] = useState(null)
-    let { id } = useParams(); 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axiosReq.get(`/corso/${id}`);
+        setName(res.data?.name);
+        setDesc(res.data?.desc);
+        setIscritti(res.data?.iscritti);
+        setImg(res.data?.img);
+        setClasse(res.data?.classe);
+        setCapienzaMassima(res.data?.capienzaMassima);
+        setDurata(res.data?.durata);
+        setSlot(res.data?.slot);
+        setOrganizzatore(res.data?.organizzatore);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    useEffect(() => {
-        const getData = async () => {
-            try{
-                const res = await axiosReq.get(`/corso/${id}`)
-                setName(res.data?.name)
-                setDesc(res.data?.desc)
-                setIscritti(res.data?.iscritti)
-                setImg(res.data?.img)
-                setClasse(res.data?.classe)
-                setCapienzaMassima(res.data?.capienzaMassima)
-                setDurata(res.data?.durata)
-                setSlot(res.data?.slot)
-                setOrganizzatore(res.data?.organizzatore)
-            } catch (error){
-                console.log(error);
-            }
-        }   
- 
-        getData()
-    }, [id])
+    getData();
+  }, [id]);
 
-    
-    //edit
-    const handleEdit = async () => {
+  //edit
+  const handleEdit = async () => {
+    if (img?.name) {
+      const fileData = await storage.ref(`corso/${img.name}`).put(img);
+      const imageSrc = await fileData.ref.getDownloadURL();
+      setImg(imageSrc);
 
-        if(img?.name){
-            const fileData = await storage.ref(`news/${img.name}`).put(img)
-            const imageSrc = await fileData.ref.getDownloadURL()
-            setImg(imageSrc)
-        }
-        try {
-            await axiosReq.put(`/corso/${id}`, {    
-                name,
-                desc,
-                iscritti,
-                img,
-                classe,
-                capienzaMassima,
-                durata,
-                slot,
-                organizzatore
-            })
-            window.location.reload()
-        } catch (error) {
-            console.log(error);
-        }
+      try {
+        await axiosReq.put(`/corso/${id}`, {
+          name,
+          desc,
+          iscritti,
+          img: imageSrc,
+          classe,
+          capienzaMassima,
+          durata,
+          slot,
+          organizzatore,
+        });
+        window.location.reload();
+      } catch (error) {
+        console.log(error);
+      }
     }
-
+  };
 
   return (
-    <div className='edit'>
-        <Navbar type="white" />
-        <div className="wrapper">
-            <h2>modifica corso</h2>
+    <div className="edit">
+      <Navbar type="white" />
+      <div className="wrapper">
+        <h2>modifica corso</h2>
 
-            <div className="edit__container">
-                <div className="edit__left">
-                    <img src={img && img} alt="" />
-                    <div className="blue-btn">
-                        <label htmlFor="coverImg">modifica immagine</label>
-                        <input type="file" name="coverImg" id="coverImg" hidden  onChange={(e) => setImg(e.target.files[0])} />
-                    </div>
-                </div>
+        <div className="edit__container">
+          <div className="edit__left">
+            <img src={img && img} alt="" />
+            <div className="blue-btn">
+              <label htmlFor="coverImg">modifica immagine</label>
+              <input
+                type="file"
+                name="coverImg"
+                id="coverImg"
+                hidden
+                onChange={(e) => setImg(e.target.files[0])}
+              />
+            </div>
+          </div>
 
-               <div className="edit__right">
-                    <div className="edit__inputs">
-                        <div className="inputGroup">
-                            <label>nome corso</label>
-                            <input type="text" placeholder={name && name} value={name && name} onChange={(e) => setName(e.target.value)} />
-                        </div>
-                        <div className="inputGroup">
-                            <label>posti disponibili totali</label>
-                            <input type="number" placeholder={capienzaMassima && capienzaMassima} value={capienzaMassima && capienzaMassima} onChange={(e) => setCapienzaMassima(e.target.value)}  />
-                        </div>
-                        <div className="inputGroup">
-                            <label>durata corso</label>
-                            <input type="number" placeholder={durata && durata} value={durata && durata} onChange={(e) => setDurata(e.target.value)}  />
-                        </div>
-                        <div className="inputGroup">
-                            <label>classe corso</label>
-                            <input type="text" placeholder={classe && classe} value={classe && classe} onChange={(e) => setClasse(e.target.value)} />
-                        </div>
-                        <div className="inputGroup">
-                            <label>slot corso</label>
-                            <input type="number" placeholder={slot && slot} value={slot && slot} onChange={(e) => setSlot(e.target.value)}   />
-                        </div>
-                    </div>
-
-                    <div className="edit__desc">
-                        <label>descrizione corso</label>
-                        <textarea cols="30" rows="10" placeholder={desc && desc} value={desc && desc} onChange={(e) => setDesc(e.target.value)}></textarea>
-                    </div>
-
-                    <div className="red-btn">
-                        <button onClick={handleEdit}>salva modifiche</button>
-                    </div>
-               </div>
+          <div className="edit__right">
+            <div className="edit__inputs">
+              <div className="inputGroup">
+                <label>nome corso</label>
+                <input
+                  type="text"
+                  placeholder={name && name}
+                  value={name && name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="inputGroup">
+                <label>posti disponibili totali</label>
+                <input
+                  type="number"
+                  placeholder={capienzaMassima && capienzaMassima}
+                  value={capienzaMassima && capienzaMassima}
+                  onChange={(e) => setCapienzaMassima(e.target.value)}
+                />
+              </div>
+              <div className="inputGroup">
+                <label>durata corso</label>
+                <input
+                  type="number"
+                  placeholder={durata && durata}
+                  value={durata && durata}
+                  onChange={(e) => setDurata(e.target.value)}
+                />
+              </div>
+              <div className="inputGroup">
+                <label>classe corso</label>
+                <input
+                  type="text"
+                  placeholder={classe && classe}
+                  value={classe && classe}
+                  onChange={(e) => setClasse(e.target.value)}
+                />
+              </div>
+              <div className="inputGroup">
+                <label>slot corso</label>
+                <input
+                  type="number"
+                  placeholder={slot && slot}
+                  value={slot && slot}
+                  onChange={(e) => setSlot(e.target.value)}
+                />
+              </div>
             </div>
 
-        </div>
-    </div>
-  )
-}
+            <div className="edit__desc">
+              <label>descrizione corso</label>
+              <textarea
+                cols="30"
+                rows="10"
+                placeholder={desc && desc}
+                value={desc && desc}
+                onChange={(e) => setDesc(e.target.value)}
+              ></textarea>
+            </div>
 
-export default Edit
+            <div className="red-btn">
+              <button onClick={handleEdit}>salva modifiche</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Edit;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,24 +1,18 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtvKTsHuB-2IZ6L0EEwFHfRhG7rmpArvo",
-  authDomain: "sito-scuola-f8b7f.firebaseapp.com",
-  projectId: "sito-scuola-f8b7f",
-  storageBucket: "sito-scuola-f8b7f.appspot.com",
-  messagingSenderId: "76398983165",
-  appId: "1:76398983165:web:4dd527934a0ef66fa88b94",
-  measurementId: "G-M94DZFJ9J7"
+  apiKey: "AIzaSyDgmRlaBNZv8f42tWHMjlOvar1GJSKdzRA",
+  authDomain: "sito-scuola-81c87.firebaseapp.com",
+  projectId: "sito-scuola-81c87",
+  storageBucket: "sito-scuola-81c87.appspot.com",
+  messagingSenderId: "833463174744",
+  appId: "1:833463174744:web:afeef669d5bc13bd1dac2d",
+  measurementId: "G-D8S8K217MY"
 };
 
-// const analytics = getAnalytics(app);
 
 
 // Initialize Firebase
